4574e50d3bf0ac614e595fbde675e4d07a48eca3
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-05-23 17:44:51 -0300

    att

---
+++ reactNativeEstudos/src/App.js
@@ -0,0 +1,28 @@
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, StatusBar } from "react-native";
+import Primeiro from "./components/Primeiro";
+import Comp, { Comp1, Comp2 } from "./components/Multi";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Primeiro />
+      <Comp />
+      <Comp1 />
+      <Comp2 />
+      <Text>Hello World!</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    // flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
