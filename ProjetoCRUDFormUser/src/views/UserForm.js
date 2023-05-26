ace57100507f640be3ccf2cbbb3c525f9f37ec5d
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-05-26 16:26:02 -0300

    att

---
+++ ProjetoCRUDFormUser/src/views/UserForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, SafeAreaView } from "react-native";
+
+const UserForm = () => {
+  return (
+    <SafeAreaView>
+      <Text>UserForm</Text>
+    </SafeAreaView>
+  );
+};
+
+export default UserForm;
---
+++ ProjetoCRUDFormUser/src/views/UserList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, SafeAreaView } from "react-native";
+
+const UserList = () => {
+  return (
+    <SafeAreaView>
+      <Text>UserList</Text>
+    </SafeAreaView>
+  );
+};
+
+export default UserList;
